8741a4d6b51e57afb831ff224a8109d45683db3f
WiktoriaNoskowicz <169058092+WiktoriaNoskowicz@users.noreply.github.com>
2026-01-04 20:30:53 +0100

    haszowanie

--- MedicalApplication/src/main/java/com/example/MedicalApplication/model/User.java
+++ MedicalApplication/src/main/java/com/example/MedicalApplication/model/User.java
@@ -2,6 +2,7 @@ package com.example.MedicalApplication.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -31,6 +32,8 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false, length = 60) 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Enumerated(EnumType.STRING)
