3466e6731f38d2b81973f5cad5a1c187865a707b
WiktoriaNoskowicz <169058092+WiktoriaNoskowicz@users.noreply.github.com>
2026-01-27 12:47:16 +0100

    Update MedicationRepository.java

--- MedicalApplication/src/main/java/com/example/MedicalApplication/repository/MedicationRepository.java
+++ MedicalApplication/src/main/java/com/example/MedicalApplication/repository/MedicationRepository.java
@@ -6,7 +6,9 @@ import com.example.MedicalApplication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +20,23 @@ public interface MedicationRepository extends JpaRepository<Medication, Long> {
 
     List<Medication> findByCreatedByDoctorTrue();
 
+
+    Optional<Medication> findByConfirmationToken(String token);
+
+    List<Medication> findByCreatedByDoctorTrueAndTherapyEndDateBefore(LocalDate date);
+
+    @Query("""
+select m from Medication m
+join fetch m.patient p
+where (m.therapyEndDate is null or m.therapyEndDate >= :today)
+""")
+    List<Medication> findActiveForNotificationsFetch(@Param("today") LocalDate today);
+
+
     //NOWE: leki dodane przez konkretnego doktora
     List<Medication> findByDoctorOrderByIntakeTimeAsc(User doctor);
+
+
 
     @Modifying
     @Query("update Medication m set m.status = :to where m.status = :from")
