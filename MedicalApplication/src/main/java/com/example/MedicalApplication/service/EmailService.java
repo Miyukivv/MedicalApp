d0f5af85694c03fbebc058947e90b649d41f0473
WiktoriaNoskowicz <169058092+WiktoriaNoskowicz@users.noreply.github.com>
2026-01-27 06:36:48 +0100

    Update EmailService.java

--- MedicalApplication/src/main/java/com/example/MedicalApplication/service/EmailService.java
+++ MedicalApplication/src/main/java/com/example/MedicalApplication/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.MedicalApplication.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,17 +13,21 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-    public void sendTestEmail() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("medicalappemailtest@gmail.com");
-        message.setTo("medicalappemailtest@gmail.com");
-        message.setSubject("Testowanie, czy działa");
-        message.setText("Jeśli przyszło, to działa!");
+    @Value("${spring.mail.username}")
+    private String from;
 
+    public void sendMedicationReminder(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
         mailSender.send(message);
     }
+
 }
